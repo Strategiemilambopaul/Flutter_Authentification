69270311dad3cc70175a06abb8a683022a5e4d22
strat mil's <pmilambo47@gmail.com>
2025-11-24 01:43:35 +0100

    update main.dart to be adapt on each platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:l4_seance_2/view/login_page%20copy.dart';
+import 'firebase_options.dart'; 
+
 import 'view/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  // Initialisation Firebase adaptée à toutes les plateformes
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
